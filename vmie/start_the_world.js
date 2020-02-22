bd74af31b508bd68ac924a606a6d5b538b6f03c0
yuguorui <yuguorui96@gmail.com>
2020-02-23 00:13:54 +0800

    进一步精简了web3代码。

--- vmie/start_the_world.js
+++ vmie/start_the_world.js
@@ -12,7 +12,7 @@ const ADDRESS = "0x000f971B010Db1038D07139fe3b1075B02ceade7";
 const PRIVATE_KEY1 = "AAE560E014720F752143833D75ECFAA0A388FBA27C49BA5C8DBC7EB862188EC9";
 const PRIVATE_KEY2 = "2701386A48D4F2D5E80E9B3E1D06D48283C386045BC5BA44DCB4F46AF6C053E1";
 
-process.on('SIGINT', ()=>{
+process.on('SIGINT', () => {
     console.log("Caught interrupt signal");
     child_process.execSync('docker-compose down');
     process.exit();
@@ -44,15 +44,16 @@ async function compileAndDeploy(filename, web3, private_key) {
 
         // 因为服务器缺乏一些特定的RPC调用接口，所以在本地签名后再发送交易。
         // Construct the raw transaction
-        // const gasLimitHex = web3.utils.toHex((await web3.eth.getBlock('latest')).gasLimit);
-        const gasLimitHex = web3.utils.toHex(300000);
-
+        const conrtact_template = new web3.eth.Contract(abi);
+        const deploy_template = conrtact_template.deploy({ data: '0x' + bytecode });
+        console.log(await web3.eth.getTransactionCount(source_address));
         const rawTx = {
             from: source_address,
             nonce: web3.utils.toHex(await web3.eth.getTransactionCount(source_address)),
+            data: deploy_template.encodeABI(),
             gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
-            gasLimit: gasLimitHex,
-            data: '0x' + bytecode,
+            gasLimit: web3.utils.toHex(await deploy_template.estimateGas())
+
         };
         console.log(rawTx);
 
@@ -85,6 +86,7 @@ async function callContractMethod(address, abi, private_key, method_name, ...arg
     const rawTx = {
         from: source_address,
         to: address,
+        nonce: web3.utils.toHex(await web3.eth.getTransactionCount(source_address, 'pending')),
         data: method_template.encodeABI(),
         gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
         gasLimit: web3.utils.toHex(await method_template.estimateGas())
@@ -117,6 +119,7 @@ async function main() {
         const web3 = new Web3();
         web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
         console.log(await compileAndDeploy('./deployer.sol', web3, PRIVATE_KEY2));
+        await sleep(10000);
         console.log(await compileAndDeploy('./deployer.sol', web3, PRIVATE_KEY2));
     } catch (error) {
         console.log('error,', error);
