9ebb515db0f3a4fd700b7776f099fa105ee3c691
yuguorui <yuguorui96@gmail.com>
2020-02-23 15:57:42 +0800

    排查eth_call的问题

--- vmie/deployer.sol
+++ vmie/deployer.sol
@@ -1,13 +1,31 @@
 pragma solidity ^0.6.1;
 
 contract Deployer {
-    function deploy() public {
+    uint public haha;
+
+    constructor() public {
+        haha = 1;
+    }
+
+    function deploy() public returns (address) {
       address addr;
       assembly {
         addr := create2(0, 0, 0, 0xFFFFFFFFFFFFFFFF)
       }
+      return addr;
     }
-    function renderHelloWorld() public pure returns (string memory) {
-        return "helloWorld";
+
+    function echo(uint num) public returns (uint) {
+        return num;
+    }
+
+    function renderHelloWorld() public returns (uint) {
+        return 42;
     }
 }
+
+// contract test {
+//     function f(uint256 a) public returns (uint256 d) {
+//         return a * 7;
+//     }
+// }
--- vmie/start_the_world.js
+++ vmie/start_the_world.js
@@ -3,14 +3,14 @@
 const fs = require('fs');
 const solc = require('solc');
 const Web3 = require('web3');
-const Tx = require('ethereumjs-tx').Transaction;
-const Common = require('ethereumjs-common').default;
 const child_process = require('child_process');
 const process = require('process');
 
 const ADDRESS = "0x000f971B010Db1038D07139fe3b1075B02ceade7";
-const PRIVATE_KEY1 = "AAE560E014720F752143833D75ECFAA0A388FBA27C49BA5C8DBC7EB862188EC9";
-const PRIVATE_KEY2 = "2701386A48D4F2D5E80E9B3E1D06D48283C386045BC5BA44DCB4F46AF6C053E1";
+
+// 需要在私钥前加入0x，以避免生成错误地址。
+const PRIVATE_KEY1 = "0xAAE560E014720F752143833D75ECFAA0A388FBA27C49BA5C8DBC7EB862188EC9";
+const PRIVATE_KEY2 = "0x2701386A48D4F2D5E80E9B3E1D06D48283C386045BC5BA44DCB4F46AF6C053E1";
 
 process.on('SIGINT', () => {
     console.log("Caught interrupt signal");
@@ -33,7 +33,7 @@ async function compileAndDeploy(filename, web3, private_key) {
             outputSelection: { '*': { '*': ['*'] } }
         }
     };
-    const source_address = web3.eth.accounts.privateKeyToAccount(private_key).address;
+    const account = web3.eth.accounts.privateKeyToAccount(private_key);
     const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
     const receipts = [];
@@ -46,85 +46,88 @@ async function compileAndDeploy(filename, web3, private_key) {
         // Construct the raw transaction
         const conrtact_template = new web3.eth.Contract(abi);
         const deploy_template = conrtact_template.deploy({ data: '0x' + bytecode });
-        console.log(await web3.eth.getTransactionCount(source_address));
-        const rawTx = {
-            from: source_address,
-            nonce: web3.utils.toHex(await web3.eth.getTransactionCount(source_address)),
+
+        const signedData = await account.signTransaction({
             data: deploy_template.encodeABI(),
-            gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
-            gasLimit: web3.utils.toHex(await deploy_template.estimateGas())
-
-        };
-        console.log(rawTx);
-
-        // Sign and serialize the transaction
-        const tx = new Tx(rawTx, {
-            common: Common.forCustomChain(
-                'mainnet',
-                {
-                    name: 'my-network',
+            gas: web3.utils.toHex(await deploy_template.estimateGas()),
+            common: {
+                customChain: {
                     networkId: 0x42,
-                    chainId: 0x42,
+                    chainId: 0x42
                 },
-                'constantinople',
-            )
+                hardfork: 'constantinople'
+            },
+            chainId: '0x42'
         });
-        tx.sign(Buffer.from(private_key, 'hex'));
-        const serializedTx = tx.serialize();
 
         // Send the transaction
-        const receipt = await web3.eth.sendSignedTransaction(serializedTx.toString('hex'));
+        const receipt = await web3.eth.sendSignedTransaction(signedData.rawTransaction);
         receipts.push({ 'receipt': receipt, 'abi': abi });
+
     }
     return receipts;
 }
 
-async function callContractMethod(address, abi, private_key, method_name, ...args) {
-    const source_address = web3.eth.accounts.privateKeyToAccount(private_key).address;
-    const contract_interface = new web3.eth.Contract(abi);
-    const method_template = contract_interface.methods[method_name](args);
-    const rawTx = {
-        from: source_address,
+async function methodSend(web3, address, abi, private_key, method_name, ...args) {
+    const account = web3.eth.accounts.privateKeyToAccount(private_key);
+    const contract_interface = new web3.eth.Contract(abi, address);
+    const method_template = contract_interface.methods[method_name](...args);
+    console.log(method_template.encodeABI());
+
+    const signedData = await account.signTransaction({
         to: address,
-        nonce: web3.utils.toHex(await web3.eth.getTransactionCount(source_address, 'pending')),
         data: method_template.encodeABI(),
-        gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
-        gasLimit: web3.utils.toHex(await method_template.estimateGas())
-    };
-    const tx = new Tx(rawTx, {
-        common: Common.forCustomChain(
-            'mainnet',
-            {
-                name: 'my-network',
+        gas: web3.utils.toHex(await method_template.estimateGas()),
+        common: {
+            customChain: {
                 networkId: 0x42,
-                chainId: 0x42,
+                chainId: 0x42
             },
-            'constantinople',
-        )
+            // hardfork: 'constantinople'
+        },
+        chainId: '0x42'
     });
-    tx.sign(Buffer.from(private_key, 'hex'));
-    const serializedTx = tx.serialize();
-    const receipt = await web3.eth.sendSignedTransaction(serializedTx.toString('hex'));
+    const receipt = await web3.eth.sendSignedTransaction(signedData.rawTransaction);
+    return receipt;
+}
+
+async function methodCall(web3, address, abi, method_name, ...args) {
+    // console.log(abi);
+    const contract_interface = new web3.eth.Contract(abi, address);
+    // web3.eth.defaultBlock = 1;
+    console.log(contract_interface.methods.renderHelloWorld().encodeABI());
+    // return await contract_interface.methods[method_name]().call();
 }
 
 async function main() {
     try {
         // start docker-compose
         const cmd = child_process.spawn('docker-compose', ['up']);
-        cmd.stdout.on('data', (msg) => { process.stdout.write(msg.toString()) });
-        cmd.stderr.on('data', (msg) => { process.stdout.write(msg.toString()) });
+        // cmd.stdout.on('data', (msg) => { process.stdout.write(msg.toString()) });
+        // cmd.stderr.on('data', (msg) => { process.stdout.write(msg.toString()) });
         await sleep(6000);
 
         // deploy the contract
         const web3 = new Web3();
         web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-        console.log(await compileAndDeploy('./deployer.sol', web3, PRIVATE_KEY2));
-        await sleep(10000);
-        console.log(await compileAndDeploy('./deployer.sol', web3, PRIVATE_KEY2));
+        const deploy_receipt = (await compileAndDeploy('./deployer.sol', web3, PRIVATE_KEY1))[0];
+
+        console.log(deploy_receipt);
+        // console.log(await methodCall(web3, deploy_receipt.receipt.contractAddress, deploy_receipt.abi, 'renderHelloWorld()'));
+        // console.log(await methodSend(web3, deploy_receipt.receipt.contractAddress, deploy_receipt.abi, PRIVATE_KEY1, 'deploy()'));
+
+        // console.log(await web3.eth.call({
+        //     to: deploy_receipt.receipt.contractAddress,
+        //     data: "0x942ae0a7",
+        //     from: ADDRESS
+        // }));
+
+        console.log(await web3.eth.getCode(deploy_receipt.receipt.contractAddress));
+        console.log(await web3.eth.getTransactionFromBlock(deploy_receipt.receipt.blockHash, 0));
     } catch (error) {
         console.log('error,', error);
     } finally {
-        child_process.execSync('docker-compose down');
+        // child_process.execSync('docker-compose down');
     }
 }
 
